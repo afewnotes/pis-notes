164e8a424bf2b513ba147fc5f2ef49dcfe9977f2
yz <yzgg1020@gmail.com>
2018-03-08 10:31:45 +0000

    object exercises

--- sfti/tmp/objects.scala
+++ sfti/tmp/objects.scala
@@ -95,16 +95,74 @@ def doWhat(color: TrafficLightColor) = {
 // TrafficLightColor.withName("Red")
 
 
+/* exercise 1 */
+object Conversions {
+    def inchesToCentimeters(inches: Double): Double = {
+        inches * 2.54
+    }
+    def gallonsToLiters(gallons: Double): Double = {
+        gallons * 3.78541178
+    }
+    def milesToKilometers(miles: Double): Double = {
+        miles * 1.609344
+    }
+}
 
+/* exercise 2 */
+class UnitConversion(val f: Double) {
+    def convert(value: Double): Double = f * value
+}
+object InchesToSantimeters extends UnitConversion(2.54)
+object GallonsToLiters extends UnitConversion(3.78541178)
+object MilesToKilometers extends UnitConversion(1.609344)
 
+/* exercise 3 */
+object Origin extends java.awt.Point
+//  object 为单例， Point 类中包含public的修改方法，不安全
 
+/* exercise 4 */
+class Point private (_x: Int = 0, _y: Int = 0) 
 
+object Point {
+    def apply(_x: Int, _y: Int)  = new Point(_x, _y)
+}
 
+// println(Point(3,4))
 
+/* exercise 5 */
+object Reverse extends App {
+    println(args.reverse.mkString(" "))
+}
 
+/* exercise 6 */
+object Cards extends Enumeration {
+    type Card = Value
+    val Spade = Value("♠")
+	val Club = Value("♣")
+	val Heart = Value("♥")
+	val Diamond = Value("♦")
+	
+// 	override def toString() = {
+// 	    Cards.values.mkString(",")
+// 	}
+/* exercise 7 */
+    def isRed(card: Card): Boolean = card == Heart || card == Diamond
+    println(for (c <- Cards.values) yield (c, isRed(c))) 
+}
 
+// println(Cards.values)
 
-
+/* exercise 8 */
+object RGBCube extends Enumeration {
+	val black = Value(0x000000, "Black")
+	val red = Value(0xff0000, "Red")
+	val green = Value(0x00ff00, "Green")
+	val blue = Value(0x0000ff, "Blue")
+	val yellow = Value(0xffff00, "Yellow")
+	val magenta = Value(0xff00ff, "Magenta")
+	val cyan = Value(0x00ffff, "Cyan")
+	val white = Value(0xffffff, "White")
+}
 
 
 
