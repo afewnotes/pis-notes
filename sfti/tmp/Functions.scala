d8f24eddbbedd64669f2a11b0e013b531418c69a
zhang-yuan <yzgg1020@gmail.com>
2018-04-04 11:06:21 +0800

    currying

--- sfti/tmp/Functions.scala
+++ sfti/tmp/Functions.scala
@@ -32,4 +32,35 @@ object Functions {
   valueAtOneQuarter((x) => 3 * x) // 省略参数类型
   valueAtOneQuarter(x => 3 * x) // 一个参数可省略括号
   valueAtOneQuarter(3 * _) // 右侧只使用到一次，用 _ 代替
+
+  (1 to 9).map("*" * _).foreach(println _)
+  (1 to 9).filter(_ % 2 == 0)
+  (1 to 9).reduceLeft(_ * _)
+  "mary had a little lamb".split(" ").sortWith(_.length < _.length)
+
+  var counter = 0
+  val button = new JButton("Increment")
+  button.addActionListener(new ActionListener {
+    override def actionPerformed(event: ActionEvent) {
+      counter += 1
+    }
+  })
+  // SAM
+  button.addActionListener(event => counter +=1) // 函数字面量起作用
+
+  val listener = (event: ActionListener) => println(counter)
+  button.addActionListener(listener) // 无法转换为 SAM 接口
+
+  val listener: ActionListener = event => println(counter) // 通过变量接收函数作为 SAM 接口
+  button.addActionListener(listener) // OK
+
+  // Currying
+  val mul = (x: Int, y: Int) => x * y
+  val mulOnAtATime = (x: Int) => ((y: Int) => x * y)
+  // def 方式定义
+  def mulOnAtATime(x: Int)(y: Int) = x * y
+  // 柯里化配合类型推导
+  val a = Array("h", "w")
+  val b = Array("H", "W")
+  a.corresponds(b)(_.equalsIgnoreCase(_))
 }
